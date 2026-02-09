8d92507838dfd3ae45c31a9e07def416a57d4365
rudrakshsattamatka-code <rudrakshsattamatka@gmail.com>
2026-02-09 17:05:33 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// PASTE YOUR FIREBASE CONFIG HERE
+// Get from: Firebase Console → Project Settings → Your Apps → Web App
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVxg4EZHm4D2zMrz_ndLzcqx3wkhdPdVc",
+  authDomain: "rudraksh-d74c3.firebaseapp.com",
+  projectId: "rudraksh-d74c3",
+  storageBucket: "rudraksh-d74c3.firebasestorage.app",
+  messagingSenderId: "270620722307",
+  appId: "1:270620722307:web:5a6454b69e12c201f6c7b6",
+  measurementId: "G-406NQEK3XV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
